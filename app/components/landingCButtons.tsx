caaeed46381ebc4ae9c36429df0e64362b6f2948
Devin Elliot <devinjelliot@gmail.com>
2023-09-29 11:45:46 -0700

    tweak mobile response

--- app/components/landingCButtons.tsx
+++ app/components/landingCButtons.tsx
@@ -1,24 +1,24 @@
 export default function LandingCButtons() {
   return (
-    <div className="grid grid-cols-4 px-20 ">
+    <div className="grid grid-cols-1 grid-rows-4 px-20 gap-2 sm:grid-cols-2 sm:grid-rows-2 sm:gap-4 md:grid-cols-4  md:gap-4 ">
       <div className="col-span-1 mx-8 rounded-lg bg-vc-border-gradient shadow-lg shadow-bl_steel-notpurp/30">
         <div className="p-3 rounded-md bg-bl_steel-charyo text-bl_steel-notpurp">
-          Accounts
+          On-Ramp
         </div>
       </div>
       <div className="col-span-1 mx-8 rounded-lg bg-vc-border-gradient shadow-lgshadow-bl_steel-notpurp/30">
         <div className="p-3 rounded-md bg-bl_steel-charyo text-bl_steel-notpurp">
-          Records
+          Off-Ramp
         </div>
       </div>
       <div className="col-span-1 mx-8 rounded-lg bg-vc-border-gradient shadow-lg shadow-bl_steel-notpurp/30">
         <div className="p-3 rounded-md bg-bl_steel-charyo text-bl_steel-notpurp">
-          Plugins
+          OTC
         </div>
       </div>
       <div className="col-span-1 mx-8 rounded-lg bg-vc-border-gradient shadow-lg shadow-bl_steel-notpurp/30">
         <div className="p-3 rounded-md bg-bl_steel-charyo text-bl_steel-notpurp">
-          Contracts
+          Paycast
         </div>
       </div>
     </div>
--- app/components/landingCard.tsx
+++ app/components/landingCard.tsx
@@ -1,18 +1,20 @@
 export default function LandingCard() {
   return (
-    <div className="px-36 pt-24 pb-20 ">
+    <div className="px-2 sm:px-20 md:px-36 pt-24 pb-20">
       <div className="rounded-3xl bg-vc-border-gradient shadow-lg shadow-black/20">
         <div className="w-auto h-auto p-4 rounded-3xl bg-bl_steel-charyo text-bl_steel-notpurp">
           <div className="h-96 rounded-lg bg-vc-border-gradient shadow-lg shadow-black/40">
-            <div className="h-96 rounded-lg border-bl_steel-charyo bg-bl_steel-graish">
-              <div className="grid grid-cols-1 px-4 space-x-4 ">
+            <div className="h-96 rounded-lg border-bl_steel-charyo bg-bl_steel-graish overflow-auto">
+              <div className="grid grid-cols-1 px-4 space-x-4">
                 <h1 className="p-4 pt-6 text-bl_steel-charyo font-semibold">
                   What is Backpack?
                 </h1>
-                <p className=" text-bl_steel-charyo">
+                <p className=" text-bl_steel-charyo ">
                   Backpack is a way to orchestrate your money. Self custody, so
-                  you are in control.
+                  you are in control. Move your money how you need to, when you
+                  want to, crazy idea right?
                 </p>
+                <p className=" text-bl_steel-charyo">- more here</p>
                 <h1 className="p-4 pt-20 justify-center text-center text-bl_steel-charyo font-semibold">
                   ** MORE COMING SOON-ISH **
                 </h1>
