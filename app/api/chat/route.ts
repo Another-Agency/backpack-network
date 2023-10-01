d9f405c2057a88ef77eaa2163bccb5c4f2ee4f87
Devin Elliot <devinjelliot@gmail.com>
2023-09-30 21:12:51 -0700

    error handling for chatg

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -12,19 +12,28 @@ const openai = new OpenAI({
 });
 
 export async function POST(req: Request) {
-  // Extract the `messages` from the body of the request
-  const { messages } = await req.json();
+  try {
+    // Extract the `messages` from the body of the request
+    const { messages } = await req.json();
+    console.log("Chat API Route", messages);
 
-  // Request the OpenAI API for the response based on the prompt
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    stream: true,
-    messages: messages,
-  });
+    // Request the OpenAI API for the response based on the prompt
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      stream: true,
+      messages: messages,
+    });
 
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response);
 
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.error("Error in POST function:", error);
+    if (error instanceof OpenAI.APIError && error.status === 405) {
+      return new Response("Method not allowed", { status: 405 });
+    }
+    throw error;
+  }
 }
